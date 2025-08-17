b6bf74b7736bf9331c8e98360a89b7e88c2e46b5
Mobolaji Richard <sinclairrichard79@gmail.com>
2025-08-17 23:54:30 +0100

    Boilerplate code added. Expo/ReactNative, NestJs, Prisma

---
+++ packages/db/index.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+
+/**
+ * This helps to create a prisma client singleton
+ */
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const prisma =
+  process.env.NODE_ENV === 'production'
+    ? new PrismaClient()
+    : global.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma;
+}
+
+export default prisma;
